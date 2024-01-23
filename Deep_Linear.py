32098a09010a288bb882fdec213934b2e307e4f8
blueforest313 <117861844+blueforest313@users.noreply.github.com>
2024-01-23 09:52:41 +0000

    Update Deep_Linear.py
    Change model_hyperparams to true_model_hyperparams

--- Deep_Linear.py
+++ Deep_Linear.py
@@ -97,7 +97,7 @@ class Bayes_Model(PyroModule):
 
 
 def generate_inputs(args):
-    inp_dim = args.model_hyperparams['dims'][0]
+    inp_dim = args.true_model_hyperparams['dims'][0]
     return 2 * args.x_max * torch.rand(args.num_data, inp_dim) - args.x_max
 
 
